4eea3b20b90576a9b4f764cbb01e91e8c1ebf80f
Paul Hondola <paulhondola@gmail.com>
2025-06-03 21:52:01 +0300

    removed project from uni repo

--- Project/src/lib/utils/utils.h
+++
@@ -1,8 +0,0 @@
-#ifndef __GLOBAL_UTILS_H
-#define __GLOBAL_UTILS_H
-
-#include <stdio.h>
-
-ssize_t read_line(int fd, char *buf, size_t max_len);
-
-#endif
--- Project/src/treasure_hub/monitor/lib_monitor/monitor_exec/monitor_exec.h
+++
@@ -1,8 +0,0 @@
-#ifndef __MONITOR_EXEC_H__
-#define __MONITOR_EXEC_H__
-
-#include "../../monitor.h"
-
-void execute_manager(const char *cmd);
-
-#endif
